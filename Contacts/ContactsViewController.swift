f0214a4b0e62c05f05aea90c9cd970df2faf3543
Ivan Shishkin <ivan23-1@yandex.ru>
2022-04-03 01:18:19 +0300

    Карта в контактах

--- Contacts/ContactsViewController.swift
+++ Contacts/ContactsViewController.swift
@@ -5,20 +5,38 @@
 //  Created by Ivan on 11.03.2022.
 //
 
+import YandexMapsMobile
 import SnapKit
 import UIKit
 
 final class ContactsViewController: UIViewController {
     
-    private let contactLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Контакты"
-        return label
-    }()
+    private let mapView = YMKMapView()
+    
+    private var mapObjects: YMKMapObjectCollection {
+        return mapView.mapWindow.map.mapObjects
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        layout()
+        setupMap()
     }
     
+    private func setupMap() {
+        let point = YMKPoint(latitude: 55.723828, longitude: 37.688591)
+        mapView.mapWindow.map.move(with: YMKCameraPosition(target: point, zoom: 15.5, azimuth: 0, tilt: 0))
+        
+        let placemark: YMKPlacemarkMapObject = mapObjects.addPlacemark(with: point) //, image: <#T##UIImage#>)
+    }
+    
+    private func layout() {
+        view.addSubview(mapView)
+        
+        mapView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.height.width.equalTo(ScreenSize.width)
+            make.left.right.equalToSuperview()
+        }
+    }
 }
--- RentalCar/AppDelegate.swift
+++ RentalCar/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Ivan on 25.01.2022.
 //
 
+import YandexMapsMobile
 import UIKit
 
 @main
@@ -13,33 +14,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupYandexMaps()
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = MainTabBarViewController()
-        
-//        window?.rootViewController = OrderUnauthorizedViewController(carModel: CarModel(
-//            marka: "KIA",
-//            model: "Rio",
-//            age: 2019,
-//            personPrice: 2450,
-//            commercialPriceWithNDS: 4000,
-//            commercialPriceWithoutNDS: 3500,
-//            driverPriceImMoscow: 2000,
-//            driverPriceInOblast: 3000,
-//            previewImage: UIImage(named: "kiaRio")!,
-//            engineVolume: "123 л.с.",
-//            numberOfSeats: 5,
-//            frontDrive: "Передний",
-//            transmission: "Автомат",
-//            numberOfDoors: 4,
-//            hasConditioner: true,
-//            allImages: [UIImage(named: "kiaRio1")!, UIImage(named: "kiaRio2")!, UIImage(named: "kiaRio3")!, UIImage(named: "kiaRio4")!],
-//            description: """
-//Новый седан Киа Рио – универсальный современный автомобиль для ежедневных задач. Оптимальная компоновка салона и максимально продуманная организация пространства подарит комфорт каждому пассажиру. Будьте уверены — места хватит всем. Оригинальный рисунок оптики украшает вид автомобиля сзади, что делает его еще более стильным. А яркий свет сделает Вас заметным в потоке даже в самый солнечный день.
-//"""
-//        ))
-        
         window?.makeKeyAndVisible()
         return true
+    }
+    
+    private func setupYandexMaps() {
+        YMKMapKit.setApiKey("562570cd-9220-4b25-bca2-be3259c844d5")
     }
 
 }
