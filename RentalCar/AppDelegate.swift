cd87ae9b4498480d1627f6e00f7c9e436e7e374b
Шишкин Иван <ivanshishkin@ozon.ru>
2023-03-30 18:40:00 +0300

    Закомент яндекс карт

--- RentalCar/AppDelegate.swift
+++ RentalCar/AppDelegate.swift
@@ -5,7 +5,7 @@
 //  Created by Ivan on 25.01.2022.
 //
 
-import YandexMapsMobile
+//import YandexMapsMobile
 import YandexMobileMetrica
 import UIKit
 
@@ -25,12 +25,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     private func setupYandexMaps() {
-        YMKMapKit.setApiKey("562570cd-9220-4b25-bca2-be3259c844d5")
+//        YMKMapKit.setApiKey("562570cd-9220-4b25-bca2-be3259c844d5")
     }
     
     private func setupYandexMetrica() {
-        guard let configuration = YMMYandexMetricaConfiguration(apiKey: "9214b44f-a95e-4515-8d91-9bf17c9fc14e") else { return }
-        YMMYandexMetrica.activate(with: configuration)
+//        guard let configuration = YMMYandexMetricaConfiguration(apiKey: "9214b44f-a95e-4515-8d91-9bf17c9fc14e") else { return }
+//        YMMYandexMetrica.activate(with: configuration)
     }
 }
 
